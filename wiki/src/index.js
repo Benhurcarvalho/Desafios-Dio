2dff19a408e8422962083ece72d6ef46032aaf2a
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2022-12-22 23:11:29 -0300

    finaliza projeto com o desafio do botão remover

--- wiki/src/index.js
+++ wiki/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App';
+import GlobalStyles  from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
